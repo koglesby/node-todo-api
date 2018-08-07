dace4d5f6f362c5a9f1f83e9c710c68274e6d1fb
Kevin Oglesby <kevinjoglesby@gmail.com>
2018-08-07 13:41:19 -0700

    added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 //Library imports
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 // Local imports
 const { mongoose } = require('./db/mongoose');
@@ -26,10 +27,30 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos})
-      .catch((e) => {
-        res.status(400).send(e);
-      })
+  }).catch((e) => {
+    res.status(400).send(e);
   });
+});
+
+// Get /todos/28282829
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    console.log("invalid");
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    // console.log(todo);
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.status(200).send({todo: todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
 });
 
 app.listen(3000, () => {
