51c80fe45090efaa538f95c4efa3cc3f770cccdf
Kevin Oglesby <kevinjoglesby@gmail.com>
2018-08-08 17:00:32 -0700

    setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config');
 //Library imports
 const _ = require('lodash');
 const express = require('express');
@@ -10,7 +11,7 @@ const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
 let app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
