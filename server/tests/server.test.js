4be3815b7fcc934a85736c8c196822da35df44e9
Kevin Oglesby <kevinjoglesby@gmail.com>
2018-10-05 13:17:32 -0700

    add DELETE /users/me/token tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -96,7 +96,7 @@ describe('GET /todos/:id', () => {
 
 describe('DELETE /todos/:id', () => {
   it('should remove a todo', done => {
-    let hexId = todos[1]._id.toHexString();
+    var hexId = todos[1]._id.toHexString();
 
     request(app)
       .delete('/todos/' + hexId)
@@ -298,6 +298,32 @@ describe('POST /users/login', () => {
           .catch(e => {
             done(e);
           });
+      });
+  });
+});
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', done => {
+    // DELETE /users/me/token
+    // Set x-auth equal to token
+    // 200
+    // Find user, verify tokens array length 0
+
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id)
+          .then(user => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          })
+          .catch(e => done(e));
       });
   });
 });
